6b91e4aa20204fa3d9288a7dda5028057059d3e3
MeowningMaster <meowningmaster@gmail.com>
2023-07-24 12:34:30 +0300

    remove junk

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import { ioc } from './ioc/index.js'
-import { partial } from './ioc/partial.js'
 import { Server } from './server/index.js'
 
-partial(ioc, {
-    controllers: ['task'],
-    services: ['database', 'notifier'],
-})
 const server = await ioc.resolve(Server)
 await server.listen()
